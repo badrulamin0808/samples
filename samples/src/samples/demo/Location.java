f343d29343d98f88431d7a0b5876b4e014c39aa6
badrulamin0808 <badrulamin1614@gmail.com>
2021-03-19 00:56:39 +0800

    Code Location

---
+++ samples/src/samples/demo/Location.java
@@ -0,0 +1,18 @@
+package samples.demo;
+
+public class Location {
+	private int height;
+	private String name;
+
+	public void print() {
+		System.out.println("height");
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+}
